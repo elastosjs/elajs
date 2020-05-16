b31e23ddc26ab18d76e7ab89f1889c8b4a4144b6
Clarence Liu <clarence.hl.liu@gmail.com>
2020-05-16 14:13:55 -0700

    update packages

--- dist/ela-js.cjs.js
+++ dist/ela-js.cjs.js
@@ -20418,11 +20418,11 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-var exports$1 = _objectSpread({
+var exports$1 = _objectSpread(_objectSpread({
   elajs: elajs,
   namehash: namehash,
   keccak256: keccak256
-}, bytesToTypes, {
+}, bytesToTypes), {}, {
   uintToBytes32: uintToBytes32
 });
 
